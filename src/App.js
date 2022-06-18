8c24e42ea0e48c8a1eea6257eafeff61503eaf66
Dennis Cahillane <dennis@thirtymadison.com>
2022-06-18 16:36:12 -0700

    convert App to a class component

--- src/App.js
+++ src/App.js
@@ -2,26 +2,29 @@ import './App.css';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import Home from './Home';
 import Arbitrage from './Arbitrage';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">🤖 MEV Degen</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#arbitrage">Arbitrage</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <Home />
-      <Arbitrage />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">🤖 MEV Degen</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#arbitrage">Arbitrage</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Home />
+        <Arbitrage />
+      </div>
+    );
+  }
 }
 
 export default App;
