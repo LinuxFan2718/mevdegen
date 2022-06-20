f22f7d3c7777a9c09a6d6dfe51bcd5c519dc8007
Dennis Cahillane <dennis@thirtymadison.com>
2022-06-20 16:49:48 -0700

    outline arbitrage card

--- src/Arbitrage.js
+++ src/Arbitrage.js
@@ -1,16 +1,39 @@
 import { Card } from 'react-bootstrap';
+import { useAddress } from "@thirdweb-dev/react";
 
 function Arbitrage() {
+  const address = useAddress();
+
   return(
-    <Card>
-      <Card.Header as="h5">Arbitrage</Card.Header>
-      <Card.Body>
-        <Card.Title>What is arbitrage?</Card.Title>
-        <Card.Text>
-          Arbitrage in MEV is taking advantage of price mismatches between decentralized exchanges to make riskless profit.
-        </Card.Text>
-      </Card.Body>
-    </Card>
+    <>
+      <Card>
+        <Card.Header as="h5">Arbitrage</Card.Header>
+        <Card.Body>
+          <Card.Title>Uniswap V2 DEX's</Card.Title>
+          <Card.Text>
+            Arbitrage in MEV is taking advantage of price mismatches between decentralized exchanges to make riskless profit.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      { address ? (
+        <Card>
+          <Card.Header as="h5">USDC-USDT</Card.Header>
+          <Card.Body>
+          <Card.Title>Quickswap</Card.Title>
+            <Card.Text>
+              price here
+            </Card.Text>
+            <Card.Title>Uniswap V2</Card.Title>
+            <Card.Text>
+              price2 here
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      ) : (
+        <>No wallet connected. This page requires a wallet to query the blockchain.</>
+      )}
+
+  </>
   )
 }
 
