f63f98dd9eaeeca41d7934b29b4bf6df3347dbe3
Dennis Cahillane <dennis@thirtymadison.com>
2022-06-20 14:03:51 -0700

    wrap App in 3rd web provider, install required npm packages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import MevDegen from './MevDegen';
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
 class App extends React.Component {
 
   render() {
     return (
-      <MevDegen />
+      <ThirdwebProvider desiredChainId={ChainId.polygonMainnet}>
+        <MevDegen />
+      </ThirdwebProvider>
     );
   }
 }
