52830a7dd08cdd6cccf0d9f691fc9a03f3528c3a
Dennis Cahillane <dennis@thirtymadison.com>
2022-06-28 16:00:39 -0700

    tighten up arbitrage card

--- src/Pair.js
+++ src/Pair.js
@@ -4,8 +4,8 @@ import pairAbi from './utils/IUniswapV2Pair.json'
 import { ethers } from "ethers";
 
 function Pair() {
-  const [reservesEx1, setReservesEx1] = useState(['loading...', 'loading...', 'loading...', 'loading...']);
-  const [reservesEx2, setReservesEx2] = useState(['loading...', 'loading...', 'loading...', 'loading...']);
+  const [reservesEx1, setReservesEx1] = useState(['loading...']);
+  const [reservesEx2, setReservesEx2] = useState(['loading...']);
   const [loading, setLoading] = useState(0);
   // exchange1 = quickswap
   const exchange1_pair_address = '0x2cF7252e74036d1Da831d11089D326296e64a728';
@@ -41,15 +41,12 @@ function Pair() {
             signer);
   
           const getReserves1Result = await exchange1PairContract.getReserves();
-          const kLastResultEx1 = await exchange1PairContract.kLast();
 
           const reserve0ex1 = getReserves1Result.reserve0;
           const reserve1ex1 = getReserves1Result.reserve1;
-          const ratioEx1 = reserve0ex1.toNumber() / reserve1ex1.toNumber();
-          const productKex1 = reserve0ex1.mul(reserve1ex1).toString();
-          const kLastEx1 = kLastResultEx1.toString();
+          const ratio01Ex1 = reserve0ex1.toNumber() / reserve1ex1.toNumber();
 
-          setReservesEx1([reserve0ex1, reserve1ex1, ratioEx1, kLastEx1, productKex1]);
+          setReservesEx1([ratio01Ex1]);
 
           // exchange 2
           const exchange2PairContract = new ethers.Contract(
@@ -58,15 +55,12 @@ function Pair() {
             signer);
   
           const getReserves2Result = await exchange2PairContract.getReserves();
-          const kLastResultEx2 = await exchange2PairContract.kLast();
 
           const reserve0ex2 = getReserves2Result.reserve0;
           const reserve1ex2 = getReserves2Result.reserve1;
-          const ratioEx2 = reserve0ex2.toNumber() / reserve1ex2.toNumber();
-          const productKex2 = reserve0ex2.mul(reserve1ex2).toString();
-          const kLastEx2 = kLastResultEx2.toString();
+          const ratio01Ex2 = reserve0ex2.toNumber() / reserve1ex2.toNumber();
 
-          setReservesEx2([reserve0ex2, reserve1ex2, ratioEx2, kLastEx2, productKex2]);          
+          setReservesEx2([ratio01Ex2]);          
 
           
         } else {
@@ -83,33 +77,28 @@ function Pair() {
   return(
     <>
       <strong>{token0} / {token1}</strong>
-      <Table striped bordered hover>
+      <Table bordered>
         <thead>
           <tr>
             <th>Exchange</th>
             <th>{token0} / {token1}</th>
-            <th>{token0}</th>
-            <th>{token1}</th>
-            <th>x * y</th>
-            <th>last k</th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <td><a href={exchange1_base_url+exchange1_pair_address+exchange1_params}>{exchange1_name}</a></td>
-            <td>{reservesEx1[2]}</td>
-            <td>{reservesEx1[0].toString()}</td>
-            <td>{reservesEx1[1].toString()}</td>
-            <td>{reservesEx1[3]}</td>
-            <td>{reservesEx1[4]}</td>
+            <td>{reservesEx1[0]}</td>
+
           </tr>
           <tr>
             <td><a href={exchange2_base_url+exchange2_pair_address+exchange2_params}>{exchange2_name}</a></td>
-            <td>{reservesEx2[2]}</td>
-            <td>{reservesEx2[0].toString()}</td>
-            <td>{reservesEx2[1].toString()}</td>
-            <td>{reservesEx2[3]}</td>
-            <td>{reservesEx2[4]}</td>
+            <td>{reservesEx2[0]}</td>
+
+          </tr>
+          <tr style={{backgroundColor: 'antiquewhite'}}>
+            <td><strong>Max Profit (before fees)</strong></td>
+            <td>${Math.abs(reservesEx1[0] - reservesEx2[0])/reservesEx1[0]}</td>
+
           </tr>
         </tbody>
       </Table>
